fb4e7bc2b7f9c1cb7b4947773213870ca56ef6ad
Rasel <rasel.aiub990@gmail.com>
2021-08-12 01:52:16 +0600

    api for employee edit,delete,update and scheduling

--- back-end/app/Http/Controllers/HRemployeeController.php
+++ back-end/app/Http/Controllers/HRemployeeController.php
@@ -38,4 +38,42 @@ class HRemployeeController extends Controller
         return response('Employees Not Found', 404)
                   ->header('Content-Type', 'text/plain');
     }
+
+    public function updateEmployee(Request $req,$employee_id)
+    {
+        $employee = Employee::where('employee_id', $employee_id)->first();
+        $employee->employee_id=$req->employee_id;
+        $employee->employee_name=$req->employee_name;
+        $employee->gender=$req->gender;
+        $employee->supervisor=$req->supervisor;
+        $employee->present_address=$req->present_address;
+        $employee->phone=$req->phone;
+        $employee->job_position=$req->job_position;
+        $employee->start_time=$req->start_time;
+        $employee->end_time=$req->end_time;
+        $employee->hour_worked=$req->hour_worked;
+        $employee->employment_start_date=$req->employment_start_date;
+        
+        $employee->save();
+        return response('Employee Updated', 200)
+                  ->header('Content-Type', 'text/plain'); 
+
+    }
+    public function deleteEmployee($employee_id)
+    {
+        if( Employee::where('employee_id', $employee_id)->delete()){
+            return response('Employee Deleted', 200)
+                  ->header('Content-Type', 'text/plain');
+        }
+        return response('Employee Not Found', 404)
+                  ->header('Content-Type', 'text/plain');                 
+    }
+
+    public function getEmpByEmpId($employee_id)
+    {
+        return Employee::where('employee_id', $employee_id)->first();
+    }
+
+   
+
 }
--- back-end/routes/erp/hr/api.php
+++ back-end/routes/erp/hr/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HRuserController;
 use App\Http\Controllers\HRemployeeController;
 use App\Http\Controllers\HRgroupController;
+use App\Http\Controllers\HRempScheduleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,10 +25,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::Post('/HR/user/create',[HRuserController::class,'addUser']);
 Route::get('/HR/user/list',[HRuserController::class,'getUserList']);
 Route::delete('/HR/user/delete/{id}',[HRuserController::class,'deleteUser']);
-Route::put("/HR/user/update/{id}",[HRuserController::class,'updateUser']);
+Route::put('/HR/user/update/{id}',[HRuserController::class,'updateUser']);
 Route::get("/HR/user/{id}",[HRuserController::class,'getUserById']);
+
 
 Route::Post('/HR/employee/create',[HRemployeeController::class,'addEmployee']);
 Route::get('/HR/employee/list',[HRemployeeController::class,'getEmployeeList']);
+Route::put('/HR/employee/update/{employee_id}',[HRemployeeController::class,'updateEmployee']);
+Route::delete('/HR/employee/delete/{employee_id}',[HRemployeeController::class,'deleteEmployee']);
 Route::Post('/HR/employee/group',[HRgroupController::class,'createGroup']);
+Route::get('/HR/employee/{employee_id}',[HRemployeeController::class,'getEmpByEmpId']);
+Route::get('/HR/emp/schedule',[HRempScheduleController::class,'schedule']);
+
+
+
+
+
 
