40b814875817d0399bc7eba653cb9456afbccffe
Atanusaha143 <atanu.saha415@gmail.com>
2021-08-11 11:30:56 +0600

    fixed create warehouse controller - backend

--- back-end/app/Http/Controllers/WarehouseController.php
+++ back-end/app/Http/Controllers/WarehouseController.php
@@ -10,18 +10,17 @@ class WarehouseController extends Controller
     public function addWarehouse(Request $req)
     {
         $warehouse = new warehouse_table;
-
         $warehouse->warehouse_id = $req->warehouse_id;
-        $warehouse->name = $req->warehouse_name;
-        $warehouse->description = $req->warehouse_description;
-        $warehouse->address = $req->warehouse_address;
-        $warehouse->zip_code = $req->warehouse_zip_code;
-        $warehouse->city = $req->warehouse_city;
-        $warehouse->country = $req->warehouse_country;
-        $warehouse->phone = $req->warehouse_phone;
-        $warehouse->status = $req->warehouse_status;
-        $warehouse->quantity = $req->warehouse_quantity;
-        $warehouse->remaining_quantity = $req->warehouse_quantity;
+        $warehouse->name = $req->name;
+        $warehouse->description = $req->description;
+        $warehouse->address = $req->address;
+        $warehouse->zip_code = $req->zip_code;
+        $warehouse->city = $req->city;
+        $warehouse->country = $req->country;
+        $warehouse->phone = $req->phone;
+        $warehouse->quantity = $req->quantity;
+        $warehouse->remaining_quantity = $req->quantity;
+        $warehouse->status = $req->status;
         $warehouse->date_added = date('Y-m-d');
         $warehouse->last_updated = date('Y-m-d');
         $result = $warehouse->save();
