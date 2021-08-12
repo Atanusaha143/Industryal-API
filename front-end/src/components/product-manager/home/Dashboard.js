6b0909947d4a1d859a8fd78a6150f891cc4aa03b
Atanusaha143 <atanu.saha415@gmail.com>
2021-08-12 11:29:32 +0600

    minor fixes to dashboard

--- front-end/src/components/product-manager/home/Dashboard.js
+++ front-end/src/components/product-manager/home/Dashboard.js
@@ -1,16 +1,28 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Table, Card } from "react-bootstrap";
+import { ScaleLoader } from "react-spinners";
 const Dashboard = () => {
   const [list, setList] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(async () => {
+    setLoading(true);
     axios
       .get("http://127.0.0.1:8000/api/product/dashboard/current-affairs")
       .then(function (response) {
         setList(response.data);
+        setLoading(false);
       });
   }, []);
+
+  const override = `
+  display: flex;
+  align-items: center;
+  justify-content: center;    
+  border-color: red;
+`;
+
   return (
     <>
       <div className="col-5 col-lg-9 border border-dark rounded p-3">
@@ -32,28 +44,41 @@ const Dashboard = () => {
                 <div className="card-body">
                   <div className="card-text">
                     <Table bordered hover>
-                      <tbody>
+                      {list.length === 0 ? (
                         <tr>
-                          <td>
-                            <b>Highest stocked product - {list[0]}</b>
+                          <td colSpan="10">
+                            <ScaleLoader
+                              css={override}
+                              size={150}
+                              color={"#eb4034"}
+                              loading={loading}
+                            />
                           </td>
                         </tr>
-                        <tr>
-                          <td>
-                            <b>Most expensive product - {list[1]}</b>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            <b>Number of good products - {list[2]}</b>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            <b>Number of faulty product - {list[3]}</b>
-                          </td>
-                        </tr>
-                      </tbody>
+                      ) : (
+                        <tbody>
+                          <tr>
+                            <td>
+                              <b>Highest stocked product - {list[0]}</b>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <b>Most expensive product - {list[1]}</b>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <b>Number of good products - {list[2]}</b>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <b>Number of faulty product - {list[3]}</b>
+                            </td>
+                          </tr>
+                        </tbody>
+                      )}
                     </Table>
                   </div>
                 </div>
