dc81d2f82ba693f7479b2f4eab5013563e63f2ca
Atanusaha143 <atanu.saha415@gmail.com>
2021-08-13 14:27:04 +0600

    Added edit profile with value field - frontend

--- front-end/src/components/product-manager/user-channel/EditProfile.js
+++ front-end/src/components/product-manager/user-channel/EditProfile.js
@@ -10,6 +10,11 @@ const EditProfile = () => {
   const MySwal = withReactContent(Swal);
   const history = useHistory();
   const [userInfo, setUserInfo] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
   const [loading, setLoading] = useState(false);
 
   useEffect(async () => {
@@ -20,8 +25,9 @@ const EditProfile = () => {
           localStorage.getItem("username")
       )
       .then(function (response) {
-        setUserInfo(response.data);
+        setUserInfo(response.data[0]);
         setLoading(false);
+        console.log(response.data);
       });
   }, []);
 
@@ -32,6 +38,8 @@ const EditProfile = () => {
   border-color: red;
 `;
 
+  const updateProfile = () => {};
+
   return (
     <>
       <div className="col-5 col-lg-9 border border-dark rounded p-3">
@@ -41,6 +49,105 @@ const EditProfile = () => {
             <hr></hr>
           </center>
           {/* component */}
+          <div className="row justify-content-center">
+            <div className="col-10">
+              <div className="container">
+                <div className="text-left">
+                  <Table striped bordered>
+                    {userInfo.length === 0 ? (
+                      <tbody>
+                        <tr>
+                          <td colSpan="10">
+                            <ScaleLoader
+                              css={override}
+                              size={150}
+                              color={"#eb4034"}
+                              loading={loading}
+                            />
+                          </td>
+                        </tr>
+                      </tbody>
+                    ) : (
+                      <tbody>
+                        <tr>
+                          <td>First Name</td>
+                          <td colSpan="2">
+                            <input
+                              type="text"
+                              className="form-control"
+                              name="firstName"
+                              value={userInfo.firstname}
+                              onChange={(e) => setFirstName(e.target.value)}
+                            ></input>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Second Name</td>
+                          <td colSpan="2">
+                            <input
+                              type="text"
+                              className="form-control"
+                              name="lastName"
+                              value={userInfo.lastname}
+                              onChange={(e) => setLastName(e.target.value)}
+                            ></input>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Email</td>
+                          <td colSpan="2">
+                            <input
+                              type="text"
+                              className="form-control"
+                              name="Email"
+                              value={userInfo.email}
+                              onChange={(e) => setEmail(e.target.value)}
+                            ></input>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Phone Number</td>
+                          <td colSpan="2">
+                            <input
+                              type="text"
+                              className="form-control"
+                              name="phoneNumber"
+                              value={userInfo.phone}
+                              onChange={(e) => setPhoneNumber(e.target.value)}
+                            ></input>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Address</td>
+                          <td colSpan="2">
+                            <textarea
+                              type="text"
+                              name="address"
+                              id=""
+                              class="form-control"
+                              value={userInfo.address}
+                              onChange={(e) => setAddress(e.target.value)}
+                            ></textarea>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td colspan="3" align="center">
+                            <button
+                              onClick={updateProfile}
+                              class="btn btn-success"
+                              style={{ width: "100%" }}
+                            >
+                              Update
+                            </button>
+                          </td>
+                        </tr>
+                      </tbody>
+                    )}
+                  </Table>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
