112b6cfe5e5943b383226afdd93fa5ddf9dfb68d
snigdho611 <snigdho.howlader@gmail.com>
2021-08-13 17:31:46 +0600

    Update SideMenu.js

--- front-end/src/components/SideMenu.js
+++ front-end/src/components/SideMenu.js
@@ -4,18 +4,18 @@ import '../App.css'
 const SideMenu = () => {
     return (
         <div id="sideMenu">
-            <div id="btn">
+            {/* <div id="btn">
                 <div id="top"></div>
                 <div id="middle"></div>
                 <div id="bottom"></div>
-            </div>
+            </div> */}
             <div id="Menu">
                 <div class="OptionList">
-                    {/* <div class="Option">Item 1</div>
+                    <div class="Option">Item 1</div>
                     <div class="Option">Item 2</div>
                     <div class="Option">Item 3</div>
                     <div class="Option">Item 4</div>
-                    <div class="Option">Item 5</div> */}
+                    <div class="Option">Item 5</div>
                 </div>
             </div>
         </div>
