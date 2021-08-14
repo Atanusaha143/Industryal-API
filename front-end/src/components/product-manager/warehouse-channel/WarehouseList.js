bb1fa4a930363606ecfff12a71366279303fe80d
Atanusaha143 <atanu.saha415@gmail.com>
2021-08-15 01:30:28 +0600

    Added search filed validation - Warehouse List

--- front-end/src/components/product-manager/warehouse-channel/WarehouseList.js
+++ front-end/src/components/product-manager/warehouse-channel/WarehouseList.js
@@ -120,23 +120,27 @@ const WarehouseList = () => {
   }
 
   const searchWarehouse = () => {
-    setLoading(true);
-    const searchUrl = "http://127.0.0.1:8000/api/warehouse/search/" + search;
-    let check = false;
-    axios
-      .get(searchUrl)
-      .then((response) => {
-        setList(response.data);
-        setLoading(false);
-        if (response.data.length === 0) {
-          setErrorMessage("Warehouse not found!");
-        } else {
-          setErrorMessage("");
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    if (search.length === 0) {
+      setErrorMessage("Please enter a Warehouse name!");
+    } else {
+      setLoading(true);
+      const searchUrl = "http://127.0.0.1:8000/api/warehouse/search/" + search;
+      let check = false;
+      axios
+        .get(searchUrl)
+        .then((response) => {
+          setList(response.data);
+          setLoading(false);
+          if (response.data.length === 0) {
+            setErrorMessage("Warehouse not found!");
+          } else {
+            setErrorMessage("");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   };
 
   return (
