acc02dfe8fe6d4757955c48bb91c5e907e7d0e50
Atanusaha143 <atanu.saha415@gmail.com>
2021-08-16 01:37:04 +0600

    Added reCAPTCHA validation

--- front-end/src/components/product-manager/user-channel/ChangePassword.js
+++ front-end/src/components/product-manager/user-channel/ChangePassword.js
@@ -2,15 +2,24 @@ import { Table } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Swal from "sweetalert2";
 import { FaWalking } from "react-icons/fa";
-import { DatasetController } from "chart.js";
+import Recaptcha from "react-recaptcha";
 
 const ChangePassword = () => {
   const [currentPass, setCurrentPass] = useState("");
   const [newPass, setNewPass] = useState("");
   const [confirmNewPass, setConfirmNewPass] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [recaptcha, setRecaptcha] = useState(false);
   const history = useHistory();
+
+  function verifyCallback(response) {
+    if (response) {
+      setRecaptcha(true);
+      console.log("OK");
+    }
+  }
 
   const proceed = () => {
     if (currentPass.length === 0) {
@@ -19,6 +28,26 @@ const ChangePassword = () => {
       setErrorMessage("New password can't be empty!");
     } else if (confirmNewPass.length === 0) {
       setErrorMessage("Confirm new password can't be empty!");
+    } else if (recaptcha === false) {
+      Swal.fire({
+        icon: "warning",
+        title: "Recaptcha needed!",
+        confirmButtonText: "Try Again",
+      });
+    } else {
+      axios
+        .get(
+          "http://127.0.0.1:8000/api/product/user/profile/" +
+            localStorage.getItem("username")
+        )
+        .then(function (response) {
+          if (response.data[0].pass === currentPass) {
+            //Proceed
+            setErrorMessage("correct current password!");
+          } else {
+            setErrorMessage("Incorrect current password!");
+          }
+        });
     }
     // else {
     //   const formData = new FormData();
@@ -65,7 +94,7 @@ const ChangePassword = () => {
                         <td clospan="3">Current Password</td>
                         <td clospan="3">
                           <input
-                            type="text"
+                            type="password"
                             class="form-control"
                             name="curr_pass"
                             onChange={(e) => setCurrentPass(e.target.value)}
@@ -76,7 +105,7 @@ const ChangePassword = () => {
                         <td clospan="3">New Password</td>
                         <td clospan="3">
                           <input
-                            type="text"
+                            type="password"
                             class="form-control"
                             name="new_pass"
                             onChange={(e) => setNewPass(e.target.value)}
@@ -87,7 +116,7 @@ const ChangePassword = () => {
                         <td clospan="3">Confirm New Password</td>
                         <td clospan="3">
                           <input
-                            type="text"
+                            type="password"
                             class="form-control"
                             name="confiem_new_pass"
                             onChange={(e) => setConfirmNewPass(e.target.value)}
@@ -95,13 +124,26 @@ const ChangePassword = () => {
                         </td>
                       </tr>
                       <tr>
-                        <td colspan="3" align="center">
+                        <td colspan="3">
+                          <div className="row justify-content-center">
+                            <div className="col-5 my-2">
+                              <Recaptcha
+                                sitekey="6LfLlS8bAAAAAOAhcu5s2mN1oTCPOkpGPApAYfxQ"
+                                render="explicit"
+                                verifyCallback={verifyCallback}
+                              />
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="mt-5" colspan="3" align="center">
                           <button
                             onClick={proceed}
                             class="btn btn-success"
                             style={{ width: "100%" }}
                           >
-                            Proceed
+                            Proceed &#8594;
                           </button>
                         </td>
                       </tr>
