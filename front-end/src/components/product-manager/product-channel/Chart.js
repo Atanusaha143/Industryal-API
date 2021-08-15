e56a220db50318e1ffe859cce5058f592362a9bd
Atanusaha143 <atanu.saha415@gmail.com>
2021-08-15 18:40:03 +0600

    Added frontend - Product Column Chart

--- front-end/src/components/product-manager/product-channel/Chart.js
+++ front-end/src/components/product-manager/product-channel/Chart.js
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Pie } from "react-chartjs-2";
+import { Pie, Bar } from "react-chartjs-2";
 import { FaTasks } from "react-icons/fa";
 
 const Chart = () => {
   const history = useHistory();
   const [types, setTypes] = useState([]);
   const [count, setCount] = useState([]);
+  const [barType, setBarTypes] = useState([]);
+  const [barCount, setBarCount] = useState([]);
 
   useEffect(() => {
     if (!localStorage.getItem("username")) {
@@ -20,6 +22,13 @@ const Chart = () => {
           setTypes(Object.keys(response.data));
           setCount(Object.values(response.data));
         });
+
+      axios
+        .get("http://127.0.0.1:8000/api/product/chart/bar")
+        .then((response) => {
+          setBarTypes(Object.keys(response.data));
+          setBarCount(Object.values(response.data));
+        });
     }
   }, []);
 
@@ -27,14 +36,44 @@ const Chart = () => {
     labels: types,
     datasets: [
       {
-        label: "Product VS Stock Pie Chart",
+        label: "Product VS Stock",
         data: count,
         backgroundColor: [
           "rgb(255, 99, 132)",
           "rgb(54, 162, 235)",
           "rgb(255, 205, 86)",
+          "rgb(255, 99, 212)",
         ],
         hoverOffset: 4,
+      },
+    ],
+  };
+
+  const barData = {
+    labels: barType,
+    datasets: [
+      {
+        label: "Product VS Price",
+        data: barCount,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
+        borderWidth: 1,
       },
     ],
   };
@@ -53,15 +92,29 @@ const Chart = () => {
           <div class="row justify-content-center mt-3">
             <div class="card w-100 bg-light text-dark border border-primary">
               <div class="card-header" align="center">
-                <b>Product wise Count</b>
+                <b>Product wise Stock</b>
               </div>
               <div class="card-body">
                 <center>
-                  <div className="col-3 m-5">
+                  <div className="col-4 m-2">
                     <Pie data={pieData} />
                   </div>
                 </center>
               </div>
+            </div>
+          </div>
+          <div class="row justify-content-center mt-3">
+            <div class="card w-100 bg-light text-dark border border-primary">
+              <div class="card-header" align="center">
+                <b>Product wise Price</b>
+              </div>
+              <center>
+                <div class="card-body">
+                  <div className="col-5 m-2">
+                    <Bar data={barData} />
+                  </div>
+                </div>
+              </center>
             </div>
           </div>
         </div>
