938fa9b4375e3b2d373943628753cb311d4f1255
snigdho611 <snigdho.howlader@gmail.com>
2021-08-16 11:03:02 +0600

    Updated a lot of the design

--- front-end/src/components/SubMenuCustomer.js
+++ front-end/src/components/SubMenuCustomer.js
@@ -5,14 +5,24 @@ import {Link, Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 
 const SideMenuCustomer = () => {
     return (
-        <div id="SideMenu">
+        <div id="SubMenu">
             <div id="Menu">
-                <div className="OptionList">
-                    <div className="Option"><Link to="/sales/customers/">All Customer</Link></div>
-                    <div className="Option"><Link to="/sales/customer/add/">Add Customer</Link></div>
-                    <div className="Option"><Link>Banned Customers</Link></div>
-                </div>
             </div>
+            <div className="SubLinkContainer">
+                <Link className="SubLink" to="/sales/customers/">
+                    All Customer
+                </Link>
+            </div>
+            <div className="SubLinkContainer">
+                <Link className="SubLink" to="/sales/customer/add/">
+                    Add Customer
+                </Link>
+            </div>
+            {/* <div className="Option">
+                <Link>
+                    Banned Customers
+                </Link>
+            </div> */}
         </div>
 
     )
