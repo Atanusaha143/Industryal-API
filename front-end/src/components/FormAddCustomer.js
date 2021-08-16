5eaab4f95e8259cb4ede4623528b2d2483d4d6f7
snigdho611 <snigdho.howlader@gmail.com>
2021-08-16 11:02:49 +0600

    Created a form for customer addition

--- front-end/src/components/FormAddCustomer.js
+++ front-end/src/components/FormAddCustomer.js
@@ -1,10 +1,40 @@
 import React from 'react'
 
 const FormAddCustomer = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Form Submitted');
+    }
+
     return (
         <div>
-            <form>
-                
+            <form className="Form" onSubmit={handleSubmit}>
+                <div className="Label-Input">
+                    <label>Name:</label>
+                    <input type="text" name="name"/>
+                </div>
+                <div className="Label-Input">
+                    <label>Email:</label>
+                    <input type="text" name="email"/>
+                    
+                </div>
+                <div className="Label-Input">
+                   <label>Phone:</label>
+                    <input type="text" name="phone"/>
+                    
+                </div>
+                <div className="Label-Input">
+                    <label>Delivery Location:</label>
+                    <input type="text" name="delivery_point"/>
+                </div>
+                <div className="Label-Input">
+                    <label>Type:</label>
+                    <input type="text" name="type"/>
+                </div>
+                <div className="Form-Button">
+                    <button>Confirm</button>
+                </div>
             </form>
         </div>
     )
