65501bdfaf421ab65996d858284aff6c2ce00bb2
Atanusaha143 <atanu.saha415@gmail.com>
2021-08-17 12:36:22 +0600

    Added singin functionality - frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import "./components/product-manager/assets/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/product-manager/home/Home";
+import Login from "./components/common/Signin";
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
+          <Route exact path="/" component={Login}></Route>
           {/* Product */}
           <Route exact path="/product">
             <Home title="dashboard" nav="common" />
