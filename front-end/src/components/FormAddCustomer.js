0be75fabd39ae18bd0d7fbb48a0d3a2ad7a820ba
snigdho611 <snigdho.howlader@gmail.com>
2021-08-18 00:48:38 +0600

    Adding form validation

--- front-end/src/components/FormAddCustomer.js
+++ front-end/src/components/FormAddCustomer.js
@@ -10,39 +10,73 @@ const FormAddCustomer = () => {
                                              phone:'', 
                                              delivery_point:'', 
                                              type:''})
-    const [error, setError] = useState([])
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors }
-      } = useForm();
+    const [errName, setErrName] = useState("");
+    const [errEmail, setErrEmail] = useState("");
+    const [errPhone, setErrPhone] = useState("");
+    const [errDeliv, setErrDeliv] = useState("");
+    const [errType, setErrType] = useState("");
 
     const handleInputChange = (event) => {
-        // const attribute = event.target.name;
-        // const val = event.target.value
-        // setFormData({...formData, [attribute]:val})
         const {name, value} = event.target
         setFormData({...formData, [event.target.name]:event.target.value})
     }
 
     const handleSubmission = (event) => {
         event.preventDefault();
-        // console.log(event.target);
-        // setFormData(event.target.value);
-        // console.log(event.target[1].value)
-        console.log(formData)
-        // if(event.target[1].value == '')
-        // {
-        //     setError()
-        // }
-        axios.post("http://127.0.0.1:8000/api/sales/customers", formData)
-            .then((response)=>{
-                console.log(response.data)
-            })
-            .catch(
-                (err)=>console.log(err.toJSON())
-            )
+        //Name
+        if(formData.name == "")
+        {
+            setErrName("Required");
+        }
+        else if(formData.name != "")
+        {
+            setErrName("");
+        }
+        //email
+        if(formData.email == "")
+        {
+            setErrEmail("Required")
+        }
+        else if(formData.email != "")
+        {
+            setErrEmail("");
+        }
+        //phone
+        if(formData.phone == "")
+        {
+            setErrPhone("Required")
+        }
+        else if(formData.phone != "")
+        {
+            setErrPhone("");
+        }
+        if(formData.delivery_point == "")
+        {
+            setErrDeliv("Required")
+        }
+        else if(formData.delivery_point != "")
+        {
+            setErrDeliv("");
+        }
+        if(formData.type == "")
+        {
+            setErrType("Required")
+        }
+        else if(formData.type != "")
+        {
+            setErrType("");
+        }
+        else
+        {
+            // axios.post("http://127.0.0.1:8000/api/sales/customers", formData)
+            // .then((response)=>{
+            //     console.log(response.data)
+            // })
+            // .catch(
+            //     (err)=>console.log(err.toJSON())
+            // )
+            console.log("Done");
+        }
         // axios.post("http://127.0.0.1:8000/api/sales/customers", {
         //     'name': event.target[0].value,
         //     'email': event.target[1].value,
@@ -61,61 +95,62 @@ const FormAddCustomer = () => {
 
     return (
         <div>
-            <form className="Form" onSubmit={handleSubmit(handleSubmission)}>
+            <form className="Form" onSubmit={handleSubmission}>
                 <div className="Label-Input">
                     <label>Name:</label>
                     <input type="text" 
                             name="name"
-                            placeholder="Please enter the customer's name"
+                            placeholder="Enter the customer's name"
                             value={formData.name}
                             onChange={handleInputChange}
-                            {...register("name", { required: true, maxLength: 20 })}
                     />
-                    {errors?.name?.type === "required" && <p>This field is required</p>} 
+                    <div className="errMsg">{errName}</div>
                 </div>
                 <div className="Label-Input">
                     <label>Email:</label>
                     <input type="text"
                             name="email"
-                            placeholder="Please enter the customer's email"
+                            placeholder="Enter the customer's email"
                             value={formData.email}
                             onChange={handleInputChange}
-                            {...register("email", { required: true, maxLength: 20 })}
+                            // {...register("email", { required: true, maxLength: 20 })}
                     />
-                    {errors?.email?.type === "required" && <p>This field is required</p>} 
+                    <div className="errMsg">{errEmail}</div>
+                    {/* {errors?.email?.type === "required" && <p>This field is required</p>}  */}
                 </div>
                 <div className="Label-Input">
                    <label>Phone:</label>
                     <input name="phone" type="text"
-                            placeholder="Please enter the customer's phone number"
+                            placeholder="Enter the customer's phone number"
                             value={formData.phone}
                             onChange={handleInputChange}
-                            {...register("phone", { required: true, maxLength: 20 })}
+                            // {...register("phone", { required: true, maxLength: 20 })}
                     />
-                    {errors?.phone?.type === "required" && <p>This field is required</p>} 
+                    <div className="errMsg">{errPhone}</div>
+                    {/* {errors?.phone?.type === "required" && <p>This field is required</p>}  */}
                     
                 </div>
                 <div className="Label-Input">
                     <label>Delivery Location:</label>
                     <input name="delivery_point" type="text"
-                            placeholder="Please enter the customer's location"
+                            placeholder="Enter the customer's location"
                             value={formData.delivery_location}
                             onChange={handleInputChange}
-                            
-                            {...register("delivery_location", { required: true, maxLength: 20 })}
+                            // {...register("delivery_location", { required: true, maxLength: 20 })}
                     />
-                    {errors?.delivery_location?.type === "required" && <p>This field is required</p>} 
+                    <div className="errMsg">{errDeliv}</div>
+                    {/* {errors?.delivery_location?.type === "required" && <p>This field is required</p>}  */}
                 </div>
                 <div className="Label-Input">
                     <label>Type:</label>
                     <input name="type" type="text"
-                            placeholder="Please enter the type of customer"
+                            placeholder="Enter the type of customer"
                             value={formData.type}
                             onChange={handleInputChange}
-                            
-                            {...register("type", { required: true, maxLength: 20 })}
+                            // {...register("type", { required: true, maxLength: 20 })}
                     />
-                    {errors?.type?.type === "required" && <p>This field is required</p>} 
+                    <div className="errMsg">{errType}</div>
+                    {/* {errors?.type?.type === "required" && <p>This field is required</p>}  */}
                 </div>
                 <div className="Form-Button">
                     <button>Confirm</button>
