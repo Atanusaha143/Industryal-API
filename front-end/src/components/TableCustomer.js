c90ec6bbec3b99a669400e19c29bfd2375e3d7fa
snigdho611 <snigdho.howlader@gmail.com>
2021-08-19 00:05:38 +0600

    Properly implemented search function

--- front-end/src/components/TableCustomer.js
+++ front-end/src/components/TableCustomer.js
@@ -12,22 +12,11 @@ const TableCustomer = () => {
         axios.get("http://127.0.0.1:8000/api/sales/customers")
             .then(response=>{
                 setItems(response.data)
-                // setError(false)
-                // setError(false)
             })
             .catch((error)=>{
-                console.log(error.request.statusText);
                 setError('404');
-                // setItems(null);
-                // setItems(null)
             }
-                // error=>setError(true)
-                // setItems(null),
-                // setError(true)
             );            
-        
-            console.log("This is error: " + error)
-            console.log("This is list: " + items)
     }, [])
 
     let id_ = 0;
@@ -35,85 +24,87 @@ const TableCustomer = () => {
     const generateUpdateForm = (id) => {
         console.log(id);
     }
+
+    const onSearchInput = (event) => {
+        event.preventDefault();
+        if(event.target.value == "")
+        {
+            axios.get("http://127.0.0.1:8000/api/sales/customers")
+            .then(response=>{
+                setItems(response.data)
+            })
+            .catch((error)=>{
+                setError('404');
+            }
+            ); 
+        }
+        else
+        {
+            console.log("Searching");
+            axios.get("http://127.0.0.1:8000/api/sales/customers/search/"+event.target.value)
+            .then(response=>{
+                setItems(response.data)
+            })
+            .catch((error)=>{
+                setItems([])
+                setError('404');
+            }
+            );    
+        }
+        console.log(event.target.value);
+                
+    }
+
     
-    if(error === '404')
-    {
-        return (
-            <div>Error receiving data</div>
-        )
-    }
-    else if(items !== null)
-    {
-        // console.log(items);
-        return (
-            <div>
-                <div className="searchBar">
-                    <form>
-                        <input type="text" className="searchBarInput" placeholder="Enter something to search"/>
-                        <button type="submit" className="searchBtn">
-                            {/* <img src="/images/search.png"/> */}
-                        </button>
-                    </form>
-                </div>
-                <table className="CusTable">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Phone</th>
-                            <th>Delivery Point</th>
-                            <th>First Purchase</th>
-                            <th>Type</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            items.map(cus=>{
-                                return (
-                                    <tr key={cus.id}>
-                                        <td>{cus.id}</td>
-                                        <td>{cus.name}</td>
-                                        <td>{cus.email}</td>
-                                        <td>{cus.phone}</td>
-                                        <td>{cus.delivery_point}</td>
-                                        <td>{cus.first_purchase}</td>
-                                        <td>{cus.type}</td>
-                                        <td><Link className="UpdateBtn" onClick={()=>generateUpdateForm(cus.id)} to={'/sales/customer/update/'+cus.id}>Update</Link></td>
-                                        <td><button>Delete</button></td>
-                                    </tr>
-                                );
-                            })
-                        }
-                    </tbody>
-                </table>
-                {/* <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1>
-                <h1>ABC</h1> */}
+    
+    
+    return (
+        <div>
+            <div className="searchBar">
+                <form>
+                    <input type="text" className="searchBarInput" placeholder="Enter Name" onChange={onSearchInput}/>
+                    <button type="submit" className="searchBtn">
+                    </button>
+                </form>
             </div>
-            
-        )
-    }
+            <table className="CusTable">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Delivery Point</th>
+                        <th>First Purchase</th>
+                        <th>Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        items
+                        ?
+                        items.map(cus=>{
+                            return (
+                                <tr key={cus.id}>
+                                    <td>{cus.id}</td>
+                                    <td>{cus.name}</td>
+                                    <td>{cus.email}</td>
+                                    <td>{cus.phone}</td>
+                                    <td>{cus.delivery_point}</td>
+                                    <td>{cus.first_purchase}</td>
+                                    <td>{cus.type}</td>
+                                    <td><Link className="UpdateBtn" onClick={()=>generateUpdateForm(cus.id)} to={'/sales/customer/update/'+cus.id}>Update</Link></td>
+                                    <td><button>Delete</button></td>
+                                </tr>
+                            );
+                        })
+                        :
+                        <div>None</div>
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default TableCustomer
