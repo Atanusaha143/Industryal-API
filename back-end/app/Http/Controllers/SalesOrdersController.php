bc3c06353a7cd5ffd52ca3efe8a9cfa49185a663
snigdho611 <snigdho.howlader@gmail.com>
2021-08-18 22:15:11 +0600

    Updating backend for orders controller

--- back-end/app/Http/Controllers/SalesOrdersController.php
+++ back-end/app/Http/Controllers/SalesOrdersController.php
@@ -28,6 +28,7 @@ class SalesOrdersController extends Controller
     {
         $request->validate([
             'customer_id'=>'required',
+            'description' => 'required',
             'total_amount'=>'required',
             'status'=>'required',
             'type'=>'required',
