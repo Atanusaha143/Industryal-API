705fa80d4574ba29133d1088d48d8ad0fbf47a3a
snigdho611 <snigdho.howlader@gmail.com>
2021-08-19 21:39:56 +0600

    Created a profile page

--- front-end/src/components/sales/Profile.js
+++ front-end/src/components/sales/Profile.js
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react'
+
 import React from 'react'
+import axios from 'axios'
+
+
 
 const Profile = () => {
+
+    const [user, setUser] = useState("")
+
+    const id = '1';
+
+    useEffect(() => {
+        axios.get("http://127.0.0.1:8000/api/sales/user/"+id)
+            .then(result=>{
+                setUser(result.data);
+            })
+            .catch((error)=>{
+                // setError("failed");
+            }
+            ); 
+            console.log(user);
+    }, [])
+
     return (
         <div class="profile">
             <div className="profilePic">
@@ -14,16 +36,16 @@ const Profile = () => {
                     <label>Phone:</label>
                     <label>Address:</label>
                     <label>Position:</label>
-                    <label>Organization ID:</label>
+                    <label>Work Hours:</label>
                 </div>
                 <div className="mainProfileData">
-                    <label>Name:</label>
-                    <label>Username:</label>
-                    <label>Email:</label>
-                    <label>Phone:</label>
-                    <label>Address:</label>
-                    <label>Position:</label>
-                    <label>Organization ID:</label>
+                    <label>{user.firstname+user.lastname}</label>
+                    <label>{user.username}</label>
+                    <label>{user.email}</label>
+                    <label>{user.phone}</label>
+                    <label>{user.address}</label>
+                    <label>{user.position}</label>
+                    <label>{user.work_hour}</label>
                 </div>
             </div>
         </div>
