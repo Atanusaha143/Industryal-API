72364e9fe572808b4449caef247280938c88a2cf
snigdho611 <snigdho.howlader@gmail.com>
2021-08-19 18:52:52 +0600

    Sales chart created for statistics

--- front-end/src/components/sales/SalesChart.js
+++ front-end/src/components/sales/SalesChart.js
@@ -2,53 +2,59 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 const data = [
     {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
+      name: 'Jan',
+      uv: 1000,
     },
     {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
+      name: 'Feb',
       uv: 2000,
-      pv: 9800,
-      amt: 2290,
     },
     {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
+      name: 'Mar',
+      uv: 3000,
     },
     {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
+      name: 'Apr',
+      uv: 4000,
     },
     {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
+      name: 'May',
+      uv: 5000,
     },
     {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
+      name: 'Jun',
+      uv: 6000,
+    },
+    {
+      name: 'Jul',
+      uv: 7000,
+    },
+    {
+      name: 'Aug',
+      uv: 8000,
+    },
+    {
+      name: 'Sep',
+      uv: 9000,
+    },
+    {
+      name: 'Oct',
+      uv: 10000,
+    },
+    {
+      name: 'Nov',
+      uv: 11000,
+    },
+    {
+      name: 'Dec',
+      uv: 12000,
     },
   ];
 
 const SalesChart = () => {
     return (
         <LineChart width={600} height={300} data={data}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <Line type="monotone" dataKey="uv"  />
         <CartesianGrid stroke="#ccc" />
         <XAxis dataKey="name" />
         <YAxis />
