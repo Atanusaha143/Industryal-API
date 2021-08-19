c4a539261949f17180fba9647ce54305166fcbc7
Atanusaha143 <atanu.saha415@gmail.com>
2021-08-20 00:13:11 +0600

    Update AddWarehouse.js

--- front-end/src/components/product-manager/warehouse-channel/AddWarehouse.js
+++ front-end/src/components/product-manager/warehouse-channel/AddWarehouse.js
@@ -45,6 +45,8 @@ const AddWarehouse = () => {
       setErrorMessage("Warehouse quantity can't be empty!");
     } else if (!Number(quantity)) {
       setErrorMessage("Warehouse quantity must be a number!");
+    } else if (quantity < 1) {
+      setErrorMessage("Please provide a valid quantity!");
     } else if (status.length === 0) {
       setErrorMessage("Warehouse status can't be empty!");
     } else {
